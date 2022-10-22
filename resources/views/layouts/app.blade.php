f1a212ec5ef2a897c6c1a2420c51fa9eb2851972
juancamilo.soto@outlook.com <juancamilo.soto@outlook.com>
2022-10-22 13:39:08 -0500

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/favicon.svg">
+
 
     <title>@yield('title', 'Directorio Operaciones')</title>
 
